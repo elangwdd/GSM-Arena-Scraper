7d7c892e821c870df3e984d03e3ada703ffd10dc
Elang Widodo <124736123+elangwdd@users.noreply.github.com>
2023-02-12 17:19:14 +0700

    Update scrape.php

--- scrape.php
+++ scrape.php
@@ -1,53 +1,53 @@
 <?php
 
-function getDeviceSpecs($device_name) {
-  $device_name = str_replace(" ", "_", $device_name);
-  $url = "https://m.gsmarena.com/" . $device_name . "-3903.php";
-  $html = file_get_contents($url);
-  $dom = new DOMDocument();
-  libxml_use_internal_errors(TRUE);
-  if(!empty($html)){
-    $dom->loadHTML($html);
-    libxml_clear_errors();
-    $xpath = new DOMXPath($dom);
-    $row = $xpath->query('//table[@data-spec="display"]/tr[3]/td[2]');
-    $display = $row->item(0)->nodeValue;
-    $row = $xpath->query('//table[@data-spec="camera"]/tr[2]/td[2]');
-    $camera = $row->item(0)->nodeValue;
-    $row = $xpath->query('//table[@data-spec="battery"]/tr[2]/td[2]');
-    $battery = $row->item(0)->nodeValue;
-    return [
-      "Display" => $display,
-      "Camera" => $camera,
-      "Battery" => $battery
-    ];
-  }
-  return null;
+$url = "https://gsmarena.com/";
+
+// Inisialisasi cURL
+$ch = curl_init();
+
+// Set opsi cURL
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Eksekusi cURL
+$data = curl_exec($ch);
+
+// Menutup cURL
+curl_close($ch);
+
+// Mencari semua tag HTML dengan class "makers"
+preg_match_all("/<li class=\"makers\"><a href=\"(.*?)\"><img src=\"(.*?)\" alt=\"(.*?)\".*?<\/li>/", $data, $matches);
+
+// Mendapatkan URL dan nama dari setiap smartphone
+$urls = $matches[1];
+$images = $matches[2];
+$names = $matches[3];
+
+// Menampilkan nama dan gambar dari setiap smartphone
+for ($i = 0; $i < count($urls); $i++) {
+    $device_url = "https://gsmarena.com" . $urls[$i];
+    $device_data = file_get_contents($device_url);
+    
+    // Mencari spesifikasi dari setiap smartphone
+    preg_match_all("/<td class=\"ttl\"><a href=\"#\".*?>(.*?)<\/a><\/td>.*?<td class=\"nfo\">(.*?)<\/td>/", $device_data, $device_matches);
+    
+    $device_specs = array();
+    for ($j = 0; $j < count($device_matches[1]); $j++) {
+        $device_specs[$device_matches[1][$j]] = $device_matches[2][$j];
+    }
+    
+    // Menampilkan nama, gambar, dan spesifikasi dari setiap smartphone
+    echo "<h3>" . $names[$i] . "</h3>";
+    echo "<img src='" . $images[$i] . "'><br><br>";
+    echo "<table>";
+    foreach ($device_specs as $spec_name => $spec_value) {
+        echo "<tr>";
+        echo "<td><b>" . $spec_name . ":</b></td>";
+        echo "<td>" . $spec_value . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    echo "<br><br>";
 }
-
-// Telegram Bot API Key
-$apiKey = "your_api_key_here";
-
-// Update URL
-$update = file_get_contents("https://api.telegram.org/bot" . $apiKey . "/getUpdates");
-$update = json_decode($update, true);
-
-// Get the chat_id and message
-$chat_id = $update["result"][count($update["result"]) - 1]["message"]["chat"]["id"];
-$message = $update["result"][count($update["result"]) - 1]["message"]["text"];
-
-// Send response
-$device_name = trim($message);
-$device_specs = getDeviceSpecs($device_name);
-if ($device_specs) {
-  $response = "Here are the specifications for the device: " . $device_name . "\n\n";
-  foreach ($device_specs as $key => $value) {
-    $response .= $key . ": " . $value . "\n";
-  }
-} else {
-  $response = "Sorry, I couldn't find any specifications for the device: " . $device_name;
-}
-
-file_get_contents("https://api.telegram.org/bot" . $apiKey . "/sendMessage?chat_id=" . $chat_id . "&text=" . urlencode($response));
 
 ?>
